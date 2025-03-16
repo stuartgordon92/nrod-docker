04298bb93f0f6b7c3e59732f0689c13bd63ceb19
stuartgordon <stuartgordon>
2025-03-16 21:01:09 +0000

    Initial Commit

---
+++ camel/src/main/java/com/example/Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("classpath:camel.xml")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
